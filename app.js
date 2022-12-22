d3365284fe1a684374260cd1e64d3865a0cdc41f
Wilkin van Roosmalen <w.s.roosmalen@gmail.com>
2022-12-22 12:35:43 +0100

    Second commit.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server)
-const port = 3000;
+const io = new Server(server);
+const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'));
 
 server.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
-})
+});

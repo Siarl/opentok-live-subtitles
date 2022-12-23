98021c43edd3ee85a853655f1f2353da0411feee
Wilkin van Roosmalen <w.s.roosmalen@gmail.com>
2022-12-23 16:33:59 +0100

    Enable speech to text through socket.io

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const { Server } = require('socket.io');
-const { SpeechClient } = require('@google-cloud/speech');
 const io = new Server(server);
 const port = process.env.PORT ?? 3000;
 const { createJwt, deleteJwt } = require('./src/helpers/googleAuth');
+const { connectionHandler } = require('./src/sockets');
 const mainRouter = require('./src/routes');
 
 async function main() {
@@ -15,36 +15,12 @@ async function main() {
     console.log(`Server is listening on port: ${port}`);
   });
 
-  const speechClient = new SpeechClient();
-
-  // The path to the remote LINEAR16 file
-  const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
-
-  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-  const audio = {
-    uri: gcsUri,
-  };
-  const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 16000,
-    languageCode: 'en-US',
-  };
-  const request = {
-    audio: audio,
-    config: config,
-  };
-
-  // Detects speech in the audio file
-  const [response] = await speechClient.recognize(request);
-  const transcription = response.results
-    .map(result => result.alternatives[0].transcript)
-    .join('\n');
-  console.log(`Transcription: ${transcription}`);
-
 }
 
 // app.use(express.static('public'));
 app.use('/', mainRouter);
+
+io.on('connected', connectionHandler);
 
 process.on('SIGINT', shutdown);
 
---
+++ src/sockets/index.js
@@ -0,0 +1,67 @@
+const io = require('socket.io');
+const speech = require('@google-cloud/speech');
+const {STT_DEFAULT_ENCODING, STT_DEFAULT_SAMPLE_RATE, STT_DEFAULT_MODEL_CONFIG, STT_INTERIM_RESULTS} = require('./stt-config')
+const speechClient = new speech.SpeechClient();
+
+const connectionHandler = (socket) => {
+
+  let recognizeStream;
+
+  socket.on('setup', (config) => {
+
+    let sampleRate = config.sampleRate ?? STT_DEFAULT_SAMPLE_RATE;
+
+    const request = {
+      config: {
+        encoding: STT_DEFAULT_ENCODING,
+        sampleRateHertz: sampleRate,
+        languageCode: STT_DEFAULT_MODEL_CONFIG.languageCode,
+        model: STT_DEFAULT_MODEL_CONFIG.model,
+        profanityFilter: STT_DEFAULT_MODEL_CONFIG.profanityFilter,
+        enableAutomaticPunctuation: STT_DEFAULT_MODEL_CONFIG.enableAutomaticPunctuation,
+      },
+      interimResults: STT_INTERIM_RESULTS, // If you want interim results, set this to true
+    };
+
+    recognizeStream = speechClient
+      .streamingRecognize(request)
+      .on('end', () => {})
+      .on('data', (data) => {
+        if (data.results[0] && data.results[0].alternatives[0]) {
+          let transcript = data.results[0].alternatives[0].transcript;
+          let isFinal = data.results[0].isFinal;
+          socket.emit('transcript', {isFinal, transcript})
+        }
+      })
+      .once('error', (event) => {
+        recognizeStream = null;
+        console.error('error-event:', event);
+      })
+      .once('close', (event) => {
+        recognizeStream = null;
+        console.error('close-event:',event);
+      })
+      .once('finish', (event) => {
+        recognizeStream = null;
+        socket.emit()
+        console.error('finish-event:',event);
+      });
+
+    socket.on('audio-data', (buffer) => {
+      if (recognizeStream) {
+        recognizeStream.write(buffer);
+      }
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (recognizeStream) {
+      recognizeStream.end();
+    }
+  });
+};
+
+
+module.exports = {
+  connectionHandler
+}
